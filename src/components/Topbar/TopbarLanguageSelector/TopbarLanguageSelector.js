fb4eefdcc08cac1029c6d3e0da37a07fda62de32
Christos Alexiou <pcnoic@gmail.com>
2023-12-23 15:45:57 +0200

    Improve language selector

--- src/components/Topbar/TopbarLanguageSelector/TopbarLanguageSelector.js
+++ src/components/Topbar/TopbarLanguageSelector/TopbarLanguageSelector.js
@@ -3,24 +3,45 @@ import styles from './TopbarLanguageSelector.module.css';
 
 const LanguageSelector = ({ currentLanguage }) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  // Define language subdomains
   const languageSubdomains = {
-    en: 'en.sekond.gr',
-    el: 'el.sekond.gr',
+    en: 'en',
+    el: 'el',
     // Add other languages and their subdomains here
   };
+
+  // Language full names
   const languageNames = {
     en: 'English',
     el: 'Ελληνικά',
     // Add other language full names here
   };
+
+  // Language flag emojis
   const languageEmojis = {
     en: '🇬🇧', // Flag emoji for English
     el: '🇬🇷', // Flag emoji for Greek
     // Add other language flag emojis here
   };
 
+  // Function to detect environment (dev or test)
+  const detectEnvironment = () => {
+    if (window.location.href.includes('.dev.')) {
+      return 'dev';
+    } else if (window.location.href.includes('.test.')) {
+      return 'test';
+    }
+    return '';
+  };
+
+  // Handle language change
   const handleLanguageChange = (languageCode) => {
-    const newUrl = `https://${languageSubdomains[languageCode]}`;
+    const environment = detectEnvironment();
+    let newUrl = `https://${languageSubdomains[languageCode]}.sekond.gr`;
+    if (environment) {
+      newUrl = `https://${languageSubdomains[languageCode]}.${environment}.sekond.gr`;
+    }
     window.location.href = newUrl;
   };
 
@@ -38,7 +59,7 @@ const LanguageSelector = ({ currentLanguage }) => {
               key={code}
               className={styles.languageOption}
               onClick={() => handleLanguageChange(code)}
-            >              
+            >
               {name}
             </div>
           ))}
